7904c55c76cdd835a3e77134ecf8c83a1c07b0f8
Angie Mojica <nataliamojica90@gmail.com>
2023-11-02 09:18:46 -0500

    Bono

--- src/main/java/edu/eci/arsw/myrestaurant/beans/TaxesCalculator.java
+++ src/main/java/edu/eci/arsw/myrestaurant/beans/TaxesCalculator.java
@@ -1,7 +1,9 @@
 package edu.eci.arsw.myrestaurant.beans;
 
-import edu.eci.arsw.myrestaurant.model.RestaurantProduct;
+import org.springframework.stereotype.Service;
 
+import edu.eci.arsw.myrestaurant.model.RestaurantProduct;
+@Service
 public interface TaxesCalculator {
 
 	public float getProductTaxes(RestaurantProduct p);
